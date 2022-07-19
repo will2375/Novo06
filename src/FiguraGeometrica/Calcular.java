9522984a148acad98b48aaec31bf5f645939479b
will2375 <wil.liam_benicio@hotmail.com>
2022-07-19 16:28:04 -0300

    Novo06

--- src/FiguraGeometrica/Calcular.java
+++ src/FiguraGeometrica/Calcular.java
@@ -6,11 +6,14 @@ public class Calcular {
         Quadrado quadrado = new Quadrado();
         quadrado.calcularArea();
 
+
         Retangulo retangulo = new Retangulo();
         retangulo.calcularArea();
 
+
         Triangulo triangulo = new Triangulo();
        triangulo.calcularArea();
+
 
         Circulo circulo = new Circulo();
        circulo.calcularArea();
